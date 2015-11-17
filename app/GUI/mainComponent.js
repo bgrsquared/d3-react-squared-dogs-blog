00cef4fdff84ad54ad4d656b8bed1d28de641f94
chroth7 <chroth@me.com>
2015-11-17 11:12:34 +0100

    Add note/warning.

--- app/GUI/mainComponent.js
+++ app/GUI/mainComponent.js
@@ -19,14 +19,25 @@ export default class mainComponent extends Component {
 
     return (<Grid fluid>
       <h1>Dog Names in Zurich</h1>
-      <small><a
+      <small><a target={'_blank'}
         href={'https://data.stadt-zuerich.ch/dataset/' +
          'pd-stapo-hundenamen/' +
           'resource/11207c68-fc9f-4883-a2ef-3b4a60dd048a'}>Source: Open Data Zurich</a>
         {' '}
-        <a href={'http://www.opendefinition.org/licenses/cc-zero'}>(under CC-Zero)</a>
+        <a target={'_blank'} href={'http://www.opendefinition.org/licenses/cc-zero'}>(under CC-Zero)</a>
       </small>
 
+      <Well bsSize={'small'}>
+        <h5>Note:</h5>
+        This page is to demonstrate a use-case
+        for <a target={'_blank'}
+               href={'https://github.com/bgrsquared/d3-react-squared'}>d3-react-squared</a>.
+        Please see TODO/Link for
+        a blog post.
+        <br/>
+        The graphs are just examples and by no means useful, regarding the information presented.
+        d3-react squared was designed to link charts, information, events.
+      </Well>
 
       <Col xs={12}>
         <p>Currently loaded: {raw.length}</p>
